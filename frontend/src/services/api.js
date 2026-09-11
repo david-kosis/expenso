2e56caa3969ef84899636b03a94076a3fccb6310
david-kosis <davidkosis64@gmail.com>
2026-09-11 18:37:07 +0100

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,5 @@
 export const API_URL =
-  "http://localhost:5000";
-
+  import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 /* =====================================================
    TOKEN
