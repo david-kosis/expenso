7eab712158081d1911504c866d50ac2e43fb635e
david-kosis <davidkosis64@gmail.com>
2026-09-12 14:17:18 +0100

    Load mobile shell fix after legacy global CSS

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "./index.css";
+import "./mobile-shell-fix.css";
 import App from "./App.jsx";
 import "./register.css";
 
